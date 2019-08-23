4ee8f3e60cd29b5f1f6ac9c11bc1b9114c714566
Davidodari <davidkibzodari@gmail.com>
2019-08-24 01:59:29 +0300

    Document main activity

--- app/src/main/java/com/davidodari/splashy/MainActivity.kt
+++ app/src/main/java/com/davidodari/splashy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.davidodari.splashy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**Host Activity for fragments**/
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
