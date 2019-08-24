037fd8b55650ca0de4420edae1775f655ac899b7
Davidodari <davidkibzodari@gmail.com>
2019-08-24 23:04:13 +0300

    Add fragment for unsplash image list view

---
+++ app/src/main/java/com/davidodari/splashy/UnsplashImagesFragment.kt
@@ -0,0 +1,21 @@
+package com.davidodari.splashy
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UnsplashImagesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_unsplash_images, container, false)
+
+
+}
