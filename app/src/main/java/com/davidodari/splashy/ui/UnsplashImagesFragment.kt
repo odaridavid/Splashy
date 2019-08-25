5e0d673f3818e71471a4160f4005fe9db72e4ac7
Davidodari <davidkibzodari@gmail.com>
2019-08-26 01:29:02 +0300

    setup staggered grid recyclerview

--- app/src/main/java/com/davidodari/splashy/ui/UnsplashImagesFragment.kt
+++ app/src/main/java/com/davidodari/splashy/ui/UnsplashImagesFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.davidodari.splashy.R
+import com.davidodari.splashy.adapter.UnsplashPhotosAdapter
+import kotlinx.android.synthetic.main.fragment_unsplash_images.view.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +20,14 @@ class UnsplashImagesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_unsplash_images, container, false)
+    ): View? {
 
+        val view = inflater.inflate(R.layout.fragment_unsplash_images, container, false)
+        view.unsplash_images_recycler_view.adapter = UnsplashPhotosAdapter()
+        val columnCount = resources.getInteger(R.integer.list_column_count)
+        view.unsplash_images_recycler_view.layoutManager =
+            StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL)
+        return view
+    }
 
 }
