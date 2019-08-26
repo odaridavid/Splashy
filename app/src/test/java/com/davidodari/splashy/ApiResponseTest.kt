af349d806685941adb0aef408872d2ab4b16d4bc
Davidodari <davidkibzodari@gmail.com>
2019-08-26 05:11:03 +0300

    add test remove dead code

---
+++ app/src/test/java/com/davidodari/splashy/ApiResponseTest.kt
@@ -0,0 +1,31 @@
+package com.davidodari.splashy
+
+import com.davidodari.splashy.api.UnsplashApiService
+import com.davidodari.splashy.api.UnsplashClient
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created By David Odari
+ * On 26/08/19
+ *
+ **/
+class ApiResponseTest {
+    //TODO Mocking
+    lateinit var unsplashService: UnsplashApiService
+
+    @Before
+    fun setup() {
+        unsplashService = UnsplashClient.getInstance()
+    }
+
+    @Test
+    fun loadImagesTest() {
+        val response = runBlocking { unsplashService.loadPhotos(page = 2, perPage = 10) }
+        val size = response.body()?.size ?: 0
+        assertTrue(size > 0)
+    }
+
+}
